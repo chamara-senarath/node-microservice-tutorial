c1ee28289e3da89161575c89f3166267ffbe2a7b
Chamara Senarath <senarath.chamara@gmail.com>
2024-03-15 14:09:59 +0000

    Added register and find service

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const app = express()
 const port = 3000
 
 const serviceRegistry = createServiceRegistry(console);
-serviceRegistry.register("serviceName", "1.0", "127.0.0.1", 8080);
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (_, res) => {
+    res.send('Hey, Try registering a new service')
 })
 
+// Register new service
+app.put('/register/:serviceName/:serviceVersion/:servicePort', (req, res) => {
+    const { serviceName, serviceVersion, servicePort } = req.params;
+    const serviceIp = req.socket.remoteAddress.includes('::') ? `[${req.socket.remoteAddress}]` : req.socket.remoteAddress;
+    const serviceKey = serviceRegistry.register(serviceName, serviceVersion, serviceIp, servicePort);
+    res.json({ serviceKey });
+})
+
+// Access a service
+app.get('/find/:serviceName/:serviceVersion', (req, res) => {
+    const { serviceName, serviceVersion } = req.params;
+    const service = serviceRegistry.getCandidate(serviceName, serviceVersion);
+    if (!service) return res.status(404).json({ result: 'Service not found' });
+    return res.json(service);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`app listening on port ${port}`)
 })
--- serviceRegistry.js
+++ serviceRegistry.js
@@ -1,26 +1,36 @@
-  const createServiceRegistry = (log) => {
-     const services = {};
+import semver from "semver";
 
-     const register = (name, version, ip, port) => {
-       const key = name + version + ip + port;
-   
-       if (!services[key]) {
-         services[key] = {};
-         services[key].timestamp = Math.floor(new Date() / 1000);
-         services[key].ip = ip;
-         services[key].port = port;
-         services[key].name = name;
-         services[key].version = version;
-         log.debug(`Added services ${name}, version ${version} at ${ip}:${port}`);
-       } else {
-         services[key].timestamp = Math.floor(new Date() / 1000);
-         log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`);
-       }
-   
-       return key;
-     };
+const createServiceRegistry = (log) => {
+  const services = {};
 
-     return { register };
- }
-  
- export default createServiceRegistry;
+  const register = (name, version, ip, port) => {
+    const key = name + version + ip + port;
+
+    if (!services[key]) {
+      services[key] = {};
+      services[key].timestamp = Math.floor(new Date() / 1000);
+      services[key].ip = ip;
+      services[key].port = port;
+      services[key].name = name;
+      services[key].version = version;
+      log.debug(`Added services ${name}, version ${version} at ${ip}:${port}`);
+    } else {
+      services[key].timestamp = Math.floor(new Date() / 1000);
+      log.debug(`Updated services ${name}, version ${version} at ${ip}:${port}`);
+    }
+
+    return key;
+  };
+
+  const getCandidate = (name, version) => {
+    console.log(name, version)
+    const candidates = Object.values(services)
+      .filter(service => service.name === name && semver.satisfies(service.version, version));
+
+    return candidates[Math.floor(Math.random() * candidates.length)];
+  }
+
+  return { register, getCandidate };
+}
+
+export default createServiceRegistry;
